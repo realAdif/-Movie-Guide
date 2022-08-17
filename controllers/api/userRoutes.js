aa976d1ffc776cec9cf0b28e632e08dcd656b7cc
ghassan <ghassanalassadi@outlook.com>
2022-08-17 18:40:20 +1000

    added some code

---
+++ controllers/api/userRoutes.js
@@ -0,0 +1,26 @@
+const router = require("express").Router();
+const { User } = require("../../models");
+
+router.post('/', async (req, res) => {
+    try {
+
+    } catch (err) {
+
+    }
+});
+
+router.post('/login', async (req, res) => {
+    try {
+
+    } catch (err) {
+
+    }
+});
+
+router.post('/logout', async (req, res) => {
+    try {
+
+    } catch (err) {
+
+    }
+});
