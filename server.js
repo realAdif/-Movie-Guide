a9f14bbb503757e6b09d991ba45079b5fca17d9a
Jason <el.jsteer@gmail.com>
2022-08-20 15:42:34 +1000

    completed code of seed.js and movies.jsona nd user.json of user and movie data

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const PORT = process.env.PORT || 3001;
 const hbs = exphbs.create({ helpers });
 
 const sess = {
-  secret: 'This is a messages',
+  secret: process.env.SECRET,
   cookie: {},
   resave: false,
   saveUninitialized: true,
@@ -37,5 +37,5 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log('Now listening'));
+  app.listen(PORT, () => console.log(`App now listening on Port: ${PORT}`));
 });
