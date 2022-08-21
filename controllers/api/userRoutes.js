939058865860a227091804ea4718cfc0f0a3e60c
Jason <el.jsteer@gmail.com>
2022-08-21 16:55:59 +1000

    Addditional bug fixes with Document name, buttons in view.handlebar files and update to paths in handlbar files and if statements in handlebar files

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -21,14 +21,18 @@ router.post('/login', async (req, res) => {
     const userData = await User.findOne({ where: { email: req.body.email } });
 
     if (!userData) {
-        res.status(400).json({ message: 'Incorrect email or password, please try again' });
+        res
+          .status(400)
+          .json({ message: 'Incorrect email or password, please try again' });
         return;
     }
 
     const validPassword = await userData.checkPassword(req.body.password);
 
     if (!validPassword) {
-        res.status(400).json({ message: 'Incorrect email or password, please try again' });
+        res
+          .status(400)
+          .json({ message: 'Incorrect email or password, please try again' });
         return;
     }
 
