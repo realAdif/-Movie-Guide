712f209b699df4e65831e06818d05ce37c2aef7b
Aditya <90451018+realAdif@users.noreply.github.com>
2022-08-23 12:30:18 +1000

    Email.js

---
+++ public/js/email.js
@@ -0,0 +1,34 @@
+const nodemailer = require("nodemailer");
+
+async function main() {
+    // Generate test SMTP service account from ethereal.email
+    // Only needed if you don't have a real mail account for testing
+    let testAccount = await nodemailer.createTestAccount();
+  
+    // create reusable transporter object using the default SMTP transport
+    let transporter = nodemailer.createTransport({
+      host: "smtp.gmail.com",
+      port: 465,
+      secure: true, // true for 465, false for other ports
+      auth: {
+        user: "moiveguide9@gmail.com", // generated ethereal user
+        pass: "MoiveGuide12345", // generated ethereal password
+      },
+    });
+  
+    // send mail with defined transport object
+    let info = await transporter.sendMail({
+      from: '"Fred Foo 👻" <foo@example.com>', // sender address
+      to: "adityagaikwad693@gmail.com",  // list of receivers
+      subject: "Hello ✔", // Subject line
+      text: "Hello world?", // plain text body
+      html: "<b>Hello world?</b>", // html body
+    });
+  
+    console.log("Message sent: %s", info.messageId);
+   
+    console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+
+}
+  
+main().catch(console.error);
