9adc2049760819007dc5c797598bf4b8c5e4f1f7
Jason <el.jsteer@gmail.com>
2022-08-24 18:31:08 +1000

    added profile.js javascript file for displaying movie profiles

---
+++ public/js/profile.js
@@ -0,0 +1,21 @@
+// const profileDisplayHandler = async (event) => {
+//   event.preventDefault();
+
+//   if () {
+//     const response = await fetch('/api/profile', {
+//       method: 'POST',
+//       body: JSON.stringify({ name, email, password, username }),
+//       headers: { 'Content-Type': 'application/json' },
+//     });
+
+//     if (response.ok) {
+//       document.location.replace('/profile');
+//     } else {
+//       alert(response.statusText);
+//     }
+//   }
+// };
+
+// document
+//   .querySelector('#topRatings')
+//   .addEventListener('click', profileDisplayHandler);
