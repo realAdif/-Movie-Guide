60da8711517fcfca257ffd454a01213290b3fa4a
Jason <el.jsteer@gmail.com>
2022-08-26 19:01:32 +1000

    cleared out code that is redundant

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -4,7 +4,6 @@ const movieRoutes = require('./movieRoutes');
 const reviewRoutes = require('./reviewRoutes');
 
 router.use('/users', userRoutes);
-router.use('/movies', movieRoutes);
 router.use('/reviews', reviewRoutes);
 
 module.exports = router;
