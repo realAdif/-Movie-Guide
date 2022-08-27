905efc42548a642db5ea5ea2e8d7b8208c6e4480
Jason <el.jsteer@gmail.com>
2022-08-27 14:42:53 +1000

    completed implementation of validator package for signup and login use

--- public/js/login.js
+++ public/js/login.js
@@ -1,12 +1,20 @@
 const loginFormHandler = async (event) => {
     event.preventDefault();
-  
+
     // Collect values from the login form
     const email = document.querySelector('#email-login').value.trim();
     const password = document.querySelector('#password-login').value.trim();
-  
-    if (email && password) {
-      // Send a POST request to the API endpoint
+
+    const validateEmail = validator.isEmail(email);
+
+    if (validator.isEmpty(email)) {
+    alert('Please enter your Email');
+    } else if (validator.isEmpty(password)) {
+    alert('Please enter a Password');
+    } else if(!validateEmail) {
+      alert("Email is not valid, Please enter a valid Email")
+    } else if (email && password) {
+    // Send a POST request to the API endpoint
       const response = await fetch('/api/users/login', {
         method: 'POST',
         body: JSON.stringify({ email, password }),
@@ -17,10 +25,11 @@ const loginFormHandler = async (event) => {
         // If successful, redirect the browser to the profile page
         document.location.replace('/profile');
       } else {
-        alert(response.statusText,"\n Incorrect Email or Password");
+        alert(`\n Incorrect Email or Password'`);
       }
-    }
-  };
+    };
+};
+
 
   document
     .querySelector('.login-form')
